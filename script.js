ed5c9806ad677db1a3453d54b4396ec490fd3c35
Khalips <khaliphilepapiyana7@gmail.com>
2026-08-27 08:35:30 +0200

    Adding Nav Bar Responsiveness

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("ProSwipe website loaded successfully.");
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+
+    if (navLinks.classList.contains("active")) {
+        menuToggle.textContent = "✕";
+    } else {
+        menuToggle.textContent = "☰";
+    }
+});
